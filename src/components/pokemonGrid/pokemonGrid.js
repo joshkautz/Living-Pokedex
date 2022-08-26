536b734846c457ce497ba8aaf7f3a24712eacc21
Emi Chen <emchen@micah-group.com>
2022-08-26 14:05:23 -0500

    bugfix: display correct caught status

--- src/components/pokemonGrid/pokemonGrid.js
+++ src/components/pokemonGrid/pokemonGrid.js
@@ -13,10 +13,9 @@ const PokemonGrid = ({ pokemon, user, pokedex }) => {
         <input type="text" name="search" placeholder="Search by Name or Number..." onChange={(e)=>setSearchQuery(e.target.value)} />
       </div>
       <div className={styles["pokemonGrid__grid"]}>
-        {pokemon.filter(d=>searchQuery === '' || d?.idx?.includes(searchQuery) || Object.values(d?.name).some(i=>i.toLowerCase().includes(searchQuery.toLowerCase()))).map((pokemon, index) => {
-          const caught = pokedex === undefined ? false : pokedex[index + 1];
+        {pokemon.filter(d=>searchQuery === '' || d?.idx?.includes(searchQuery) || Object.values(d?.name).some(i=>i.toLowerCase().includes(searchQuery.toLowerCase()))).map((p, index) => {
           return (
-            <PokemonCard key={index} pokemon={pokemon} user={user} caught={caught} />
+            <PokemonCard key={index} pokemon={p} user={user} caught={(p?.idx && pokedex) ? pokedex[parseInt(p?.idx)] : false } />
           );
         })}
       </div>
